7c4b231560272c69b05a676e926ee0c52e488d96
janyousssef <89477@must.edu.eg.com>
2023-03-14 19:10:37 +0200

    rename class

--- src/main/java/com/jan/fileuploadexample/FileUploadExampleApplication.java
+++ src/main/java/com/jan/fileuploadexample/FileUploadExampleApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan
 @SpringBootApplication
-public class FileuploadexampleApplication {
+public class FileUploadExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FileuploadexampleApplication.class, args);
+		SpringApplication.run(FileUploadExampleApplication.class, args);
 	}
 
 }
